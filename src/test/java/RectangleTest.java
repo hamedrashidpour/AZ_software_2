1e5ed8b198a3b7eb48e8be775ec89c4c7c379120
hamedrashidpour <hamedrashid1421@gmail.com>
2023-07-29 21:30:02 +0330

    add getAndSetTest method

--- src/test/java/RectangleTest.java
+++ src/test/java/RectangleTest.java
@@ -9,6 +9,10 @@ public class RectangleTest {
         assertEquals(10, rectangle.computeArea(5, 2));
         assertEquals(6, rectangle.computeArea(3, 2));
         assertEquals(12, rectangle.computeArea(6, 2));
-
+    }
+    @Test
+    public void getAndSetTest(){
+        Rectangle rectangle = new Rectangle();
+        assertEquals(5, rectangle.height = 5);
     }
 }
