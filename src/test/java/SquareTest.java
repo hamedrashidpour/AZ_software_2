edf3a0ce7165789712fb4a85b23c38975bf3fe4b
hamedrashidpour <hamedrashid1421@gmail.com>
2023-07-30 11:32:42 +0330

    add test to computeAreaTest method

--- src/test/java/SquareTest.java
+++ src/test/java/SquareTest.java
@@ -11,4 +11,10 @@ public class SquareTest {
         assertEquals(5.0, square.getSide(), 0);
         assertEquals(4.0, new Square(4.0).getSide(), 0);
     }
+
+    @Test
+    public void computeAreaTest(){
+        Square square = new Square(5);
+        assertEquals(25.0, square.computeArea(), 0);
+    }
 }
