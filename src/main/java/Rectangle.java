4c3dd43b99bbb3975cc20fa357a9e46d6c59875e
hamedrashidpour <hamedrashid1421@gmail.com>
2023-07-30 08:43:52 +0330

    refactor Rectangle class

--- src/main/java/Rectangle.java
+++ src/main/java/Rectangle.java
@@ -1,9 +1,25 @@
 public class Rectangle {
-    public int height;
-    public int width;
+    private int height;
+    private int width;
     public Rectangle() {
     }
-    public long computeArea(int height,int width){
-        return (long) height * width;
+    public long computeArea(){
+        return (long) this.height * this.width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
     }
 }
--- src/test/java/RectangleTest.java
+++ src/test/java/RectangleTest.java
@@ -6,16 +6,26 @@ public class RectangleTest {
     @Test
     public void  computeAreaTest(){
         Rectangle rectangle = new Rectangle();
-        assertEquals(10, rectangle.computeArea(5, 2));
-        assertEquals(6, rectangle.computeArea(3, 2));
-        assertEquals(12, rectangle.computeArea(6, 2));
+        rectangle.setHeight(5);
+        rectangle.setWidth(2);
+        assertEquals(10, rectangle.computeArea());
+        rectangle.setHeight(3);
+        rectangle.setWidth(2);
+        assertEquals(6, rectangle.computeArea());
+        rectangle.setHeight(6);
+        rectangle.setWidth(2);
+        assertEquals(12, rectangle.computeArea());
     }
     @Test
     public void getAndSetTest(){
         Rectangle rectangle = new Rectangle();
-        assertEquals(5, rectangle.height = 5);
-        assertEquals(10, rectangle.height = 10);
-        assertEquals(4, rectangle.width = 4);
-        assertEquals(8, rectangle.width = 8);
+        rectangle.setHeight(5);
+        assertEquals(5, rectangle.getHeight());
+        rectangle.setHeight(10);
+        assertEquals(10, rectangle.getHeight());
+        rectangle.setWidth(4);
+        assertEquals(4, rectangle.getWidth());
+        rectangle.setWidth(8);
+        assertEquals(8, rectangle.getWidth());
     }
 }
