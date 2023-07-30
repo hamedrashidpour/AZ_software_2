d08014c593d28952c2a63e575af5d81a519d333b
hamedrashidpour <hamedrashid1421@gmail.com>
2023-07-30 11:16:17 +0330

    create SquareTest class and add first test

---
+++ src/test/java/SquareTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.assertEquals;
+
+
+import org.junit.Test;
+
+public class SquareTest {
+    @Test
+    public void SquareSideTest(){
+        assertEquals(5.0, Square.side = 5.0, 0);
+    }
+}
